a3162ee2d6cad013c8ff1b37639b74b25ce1c7a7
Swaril Singhal <swaril.singhal@saavn.com>
2022-09-07 00:16:40 +0530

    minor fix

--- main.py
+++ main.py
@@ -20,12 +20,12 @@ app.secret_key = "testing"
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
-mongo_uri = "mongodb://swaril:" + urllib.parse.quote(
-    "$w@R!1") + "@ac-ymz3eon-shard-00-00.iympypo.mongodb.net:27017,ac-ymz3eon-shard-00-01.iympypo.mongodb.net:27017,ac-ymz3eon-shard-00-02.iympypo.mongodb.net:27017/?ssl=true&replicaSet=atlas-y20jq1-shard-0&authSource=admin&retryWrites=true&w=majority"
-client = pymongo.MongoClient(
-    mongo_uri)
-db = client.User
-records = db.cashManagement
+# mongo_uri = "mongodb://swaril:" + urllib.parse.quote(
+#     "$w@R!1") + "@ac-ymz3eon-shard-00-00.iympypo.mongodb.net:27017,ac-ymz3eon-shard-00-01.iympypo.mongodb.net:27017,ac-ymz3eon-shard-00-02.iympypo.mongodb.net:27017/?ssl=true&replicaSet=atlas-y20jq1-shard-0&authSource=admin&retryWrites=true&w=majority"
+# client = pymongo.MongoClient(
+#     mongo_uri)
+# db = client.User
+# records = db.cashManagement
 # video_stream = VideoCamera()
 
 # db = client.get_database('total_records')
